1f7e257ee579d24077746e32108b2afb72abd269
safarawolaz <safarawolaz@yahoo.com>
2022-06-22 21:07:43 +0200

    hero animation

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import { init } from 'ityped'
 import styles from '../styles/Hero.module.css'
+import { useEffect, useRef } from 'react'
 
 const Hero = () => {
+  const textRef = useRef()
+  
+    useEffect(() => {
+      init(textRef.current, { 
+        showCursor: true,
+        backDelay: 2000,
+        backSpeed: 150,
+        strings: ['Web Development','Graphic Design', 'Digital Marketing', 'Mobile Development', 'IT Support', 'SEO'] 
+      })
+    },[])
+
   return (
     <div className={styles.container}>
     <div className={styles.videoContainer}>
@@ -10,7 +23,7 @@ const Hero = () => {
       </div>
      <div className={styles.heroContent}>
         <p className={styles.subHeading}>your trusted partner in it</p>
-        <h1 className={styles.heading}>Digital Marketing</h1>
+        <h1 className={styles.heading } ref={textRef}></h1>
         <button className={styles.btn}>Get a free quote</button>
     </div>  
     </div>
