1b81fc2caec80ba359b3f46f19e4f6cff163ba27
safarawolaz <safarawolaz@yahoo.com>
2022-06-22 02:13:18 +0200

    added nav menu for mobile

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/Navbar.module.css'
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false)
   return (
     <div className={styles.container}>
         {/* <div class="fas fa-bars" id="menu-btn"></div> */}
@@ -25,6 +26,28 @@ const Navbar = () => {
                 <Link  href="/blogs">blogs</Link>
               </li>
               <li className={styles.navLink}>
+                <Link  href="/contactUs">contact us</Link>
+              </li>
+        </ul>
+        <div className={styles.hamburger} onClick={() => setOpen(!open)}>
+            <div className={styles.line}></div>
+            <div className={styles.line}></div>
+            <div className={styles.line}></div>
+        </div>
+        <ul onClick={() => setOpen(false)} className={styles.menu} style={{right: open ? "0px" : "-50vw"}}>
+              <li className={styles.menuItem}>
+                <Link  href="/about">about us</Link>
+              </li>
+              <li className={styles.menuItem}>
+                <Link  href="/services">services</Link>
+              </li>
+              <li className={styles.menuItem}>
+                <Link  href="/projects">projects</Link>
+              </li>
+              <li className={styles.menuItem}>
+                <Link  href="/blogs">blogs</Link>
+              </li>
+              <li className={styles.menuItem}>
                 <Link  href="/contactUs">contact us</Link>
               </li>
         </ul>
